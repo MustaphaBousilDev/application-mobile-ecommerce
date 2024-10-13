6c4c64b305129cc9857eadac283c6e636871cb49
Mustapha bousil <bousilmustapha@gmail.com>
2024-10-13 01:30:49 +0100

    cleaning code in Component FilterSorting

--- ecommerce/components/home/FilterSorting.tsx
+++ ecommerce/components/home/FilterSorting.tsx
@@ -6,28 +6,45 @@ import AntDesign from '@expo/vector-icons/AntDesign';
 const FilterSorting = () => {
   const [filtering, setFiltering] = useState(false)
   const [sorting, setSorting] = useState(false)
-  const handleSorting = () => {}
-  const handleFiltering = () => {}
+  const handleSorting = () => setSorting(prev => !prev)
+  const handleFiltering = () => setFiltering(prev => !prev)
+  const buttonStyles = (isActive: boolean) => ({
+    backgroundColor: isActive ? '#333' : '#fff',
+    color: isActive ? '#fff' : '#333',
+  })
+  const renderButton = (isActive: boolean, label: string, icon: JSX.Element, onPress: () => void) => (
+    <TouchableOpacity
+      style={[styles.button, buttonStyles(isActive)]}
+      onPress={onPress}
+    >
+      <Text style={{ color: buttonStyles(isActive).color }}>{label}</Text>
+      <Text>{icon}</Text>
+    </TouchableOpacity>
+  );
   return (
     <View style={styles.container}>
       <View style={styles.left}>
         <Text style={styles.title}>All Features</Text>
       </View>
       <View style={styles.right}>
-        <TouchableOpacity 
-          style={styles.button}
-          onPress={handleSorting}
-        >
-            <Text>Sort</Text>
-            <Text><MaterialIcons name="sort" size={22} color="black" /></Text>
-        </TouchableOpacity>
-        <TouchableOpacity  
-          style={styles.button}
-          onPress={handleFiltering}
-        >
-            <Text>Filter</Text>
-            <Text><AntDesign name="filter" size={22} color="black" /></Text>
-        </TouchableOpacity>
+        {renderButton(
+            sorting,
+            'Sort',
+            <MaterialIcons name="sort" 
+              size={22} 
+              color={buttonStyles(sorting).color} 
+            />,
+            handleSorting
+        )}
+        {renderButton(
+            filtering,
+            'Filter',
+            <AntDesign 
+              name="filter" 
+              size={22} 
+              color={buttonStyles(filtering).color} />,
+              handleFiltering
+        )}
       </View>
     </View>
   )
@@ -50,7 +67,7 @@ const styles = StyleSheet.create({
         flexDirection:'row',
         gap:3,
         alignItems:'center',
-        backgroundColor:'#fff',
+        
         padding:7,
         paddingVertical:3,
         borderRadius:7,
