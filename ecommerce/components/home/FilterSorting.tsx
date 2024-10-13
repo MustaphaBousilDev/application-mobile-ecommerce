ff8fddfefa6439952df355af23e0012a5a565fc1
Mustapha bousil <bousilmustapha@gmail.com>
2024-10-13 01:18:32 +0100

    add functions press

--- ecommerce/components/home/FilterSorting.tsx
+++ ecommerce/components/home/FilterSorting.tsx
@@ -1,20 +1,30 @@
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import AntDesign from '@expo/vector-icons/AntDesign';
 
 const FilterSorting = () => {
+  const [filtering, setFiltering] = useState(false)
+  const [sorting, setSorting] = useState(false)
+  const handleSorting = () => {}
+  const handleFiltering = () => {}
   return (
     <View style={styles.container}>
       <View style={styles.left}>
         <Text style={styles.title}>All Features</Text>
       </View>
       <View style={styles.right}>
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity 
+          style={styles.button}
+          onPress={handleSorting}
+        >
             <Text>Sort</Text>
             <Text><MaterialIcons name="sort" size={22} color="black" /></Text>
         </TouchableOpacity>
-        <TouchableOpacity  style={styles.button}>
+        <TouchableOpacity  
+          style={styles.button}
+          onPress={handleFiltering}
+        >
             <Text>Filter</Text>
             <Text><AntDesign name="filter" size={22} color="black" /></Text>
         </TouchableOpacity>
